d5324caf8c2116523b2fc28756d36a2985ce27c5
ShreyasAiyar <shreyas.aiyar96@gmail.com>
2020-11-16 17:11:54 -0500

    Added basic query algorithm

---
+++ query.py
@@ -0,0 +1,82 @@
+import sys
+from six_frame_translation import six_frame_translation
+from collections import defaultdict
+from utils import *
+
+KMER_FILE = 'kmer_dict_k_4_num_prots_2.pickle'
+UNIPROT_FILE = 'uniprot_sprot.fasta'
+
+def query(p):
+    """ Function that outputs (approximate) matches of a DNA read against 
+    a protein database. Implemented using k-mers and the pigeonhole principle.
+
+    Assumes number of mismatches is fixed (4)
+    Assumes length of p is fixed (60)
+    Does not account for gaps or insertions (only mismatches)
+    """
+    assert (len(p) == 60)
+
+    # kmer_dict holds mapping from {kmer: [(protein_id, offset)]}
+    kmer_dict = read_from_pickle(KMER_FILE)
+
+    # protein_dict holds mapping from {protein_id : protein_seq}
+    protein_dict = create_protein_map(UNIPROT_FILE, 1)
+
+    translations = six_frame_translation(p)
+    mm = 4
+    
+    # Result stored as a set of tuples (id, offset)
+    occurrences = set()
+
+    for translation in translations:
+        for i in range(5):
+            off = 4*i
+            part = translation[off:off+4]
+
+            if part not in kmer_dict:
+                continue
+            
+            hits = kmer_dict[part]
+            # hits is a list of [(protein_id, offset)]
+            for (id, offset) in hits:
+                t = protein_dict[id]
+                assert(len(t) != 0)
+                nmm = 0
+                if offset - off + len(translation) <= len(t) and offset - off >= 0:
+                    for j in range(0, len(translation)): 
+                        if t[offset-off+j] != translation[j]:
+                            nmm += 1
+                            if nmm > mm:
+                                break
+                    if nmm <= mm:
+                        occurrences.add((id, offset-off))  
+    return occurrences
+
+# Test Cases
+def main():
+
+    p = "ATGGCGTTTAGCGCGGAAGATGTGCTGAAAGAATATGATCGCCGCCGCCGCATGGAAGCG"
+
+    # 1 Mismatch - should pass
+    occ = query("TTGGCGTTTAGCGCGGAAGATGTGCTGAAAGAATATGATCGCCGCCGCCGCATGGAAGCG")
+    assert(("Q6GZX4", 0) in occ)
+
+    # 2 Mismatches - should pass
+    occ = query("TTTGCGTTTAGCGCGGAAGATGTGCTGAAAGAATATGATCGCCGCCGCCGCATGGAAGCG")
+    assert(("Q6GZX4", 0) in occ)
+
+    # 3 Mismatches - should pass
+    occ = query("TTTGCGTTTAGCGCGGAAGATGTTCTGAAAGAATATGATCGCCGCCGCCGCATGGAAGCG")
+    assert(("Q6GZX4", 0) in occ)
+
+    # 4 Mismatches - should pass
+    occ = query("TTTGCGTTTAGCGCGGAAGATGTTCTGAGAGAATATGATCGCCGCCGCCGCATGGAAGCG")
+    assert(("Q6GZX4", 0) in occ)
+
+    # > 4 Mismatches - should fail
+    occ = query("TTGCTTTAGCGCGGAAGATGTTCTGAGAGAAGTATGATCGCCGCCGCCACATGGAAGCTG")
+    assert(("Q6GZX4", 0) not in occ)
+
+
+if __name__ == "__main__":
+    main()
