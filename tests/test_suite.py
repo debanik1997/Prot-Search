e33966b0c95d6fbf29e81f193bde8365a12ba8f3
Ishita Tripathi <sakurakhadag@gmail.com>
2020-12-08 23:43:13 -0500

    Change test suite to take k and mm as inputs

--- tests/test_suite.py
+++ tests/test_suite.py
@@ -2,7 +2,7 @@
 Prints a summary of execution time and memory usage
 
 Example
-python3 test_suite.py --infile test_reads.txt
+python3 test_suite.py --infile test_reads.txt -mm 4 -l 4
 """
 
 import argparse
@@ -25,7 +25,7 @@ def read_from_pickle(f):
     return dictionary
 
 def profile(func):
-    def wrapper(reads, kmer_dict, protein_dict):
+    def wrapper(reads, kmer_dict, protein_dict, mm, l):
         tracemalloc.start()
         start_time = timer()
         func(reads, kmer_dict, protein_dict)
@@ -34,7 +34,7 @@ def profile(func):
         print(f"Peak Memory Usage: {peak / 10**3}KB")
         avg_time = (end_time - start_time) / len(reads) * 10**3
         print(f"Average Execution Time: {avg_time:.2f}ms \n")
-    
+
     return wrapper
 
 def print_occurences(read, occ):
@@ -44,10 +44,11 @@ def print_occurences(read, occ):
         print(f"{protein_id} \t {offset} \t \t {v}")
 
 @profile
-def pigeonhole_test(reads, kmer_dict, protein_dict):
+def pigeonhole_test(reads, kmer_dict, protein_dict, mm = 4, l=4):
     print("l-mer Filtration Algorithm:")
     for read in reads:
-        occ = pigeonhole_query(read, kmer_dict, protein_dict)
+        # change here!
+        occ = pigeonhole_query(read, kmer_dict, protein_dict, max_mismatch = mm)
         if len(occ) == 0:
             print("No occurrences found")
             continue
@@ -56,10 +57,10 @@ def pigeonhole_test(reads, kmer_dict, protein_dict):
         print_occurences(read, occ)
 
 @profile
-def index_assisted_dp_test(reads, kmer_dict, protein_dict):
+def index_assisted_dp_test(reads, kmer_dict, protein_dict, mm = 4, l=4):
     print("Index Assisted DP Algorithm:")
     for read in reads:
-        occ = index_assisted_query(read, kmer_dict, protein_dict)
+        occ = index_assisted_query(read, kmer_dict, protein_dict, max_mismatch=mm, l=l)
         if len(occ) == 0:
             print("No occurrences found")
             continue
@@ -67,23 +68,31 @@ def index_assisted_dp_test(reads, kmer_dict, protein_dict):
         print(f"Id \t Offset \t Mismatches/Gaps/Insertions")
         print_occurences(read, occ)
 
-def run_test_suite(infile, kmer_dict, protein_dict):
+def run_test_suite(infile, kmer_dict, protein_dict, mm = 4, l = 4):
     with open(infile) as f:
         reads = f.readlines()
     reads = [read.strip("\n") for read in reads]
-    pigeonhole_test(reads, kmer_dict, protein_dict)
-    index_assisted_dp_test(reads, kmer_dict, protein_dict)
+    pigeonhole_test(reads, kmer_dict, protein_dict, mm, l)
+    index_assisted_dp_test(reads, kmer_dict, protein_dict, mm, l)
 
 def main():
     ap = argparse.ArgumentParser()
     ap.add_argument('--infile', type=str, required=True,
                     help='path to test_reads file')
+    ap.add_argument('-l', type = str, default = '4',
+                    help='length of lmer')
+    ap.add_argument('--mm', type = int, default = 4,
+                    help='Maximum mismatches allowed for pigeonhole')
     args = ap.parse_args()
     infile = args.infile
+    l = args.l
+    mm = args.mm
+
+    KMER_FILE = '../data/kmer_dict_k_' + l + '_num_prots_100.pickle'
     kmer_dict = read_from_pickle(KMER_FILE)
     protein_dict = read_from_pickle(PROTEIN_FILE)
 
-    run_test_suite(infile, kmer_dict, protein_dict)
+    run_test_suite(infile, kmer_dict, protein_dict, mm, int(l))
 
 if __name__ == "__main__":
     main()
