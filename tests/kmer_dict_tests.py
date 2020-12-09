b3d217e63ec9e7ea49a534a728962b8b335c9041
Ishita Tripathi <sakurakhadag@gmail.com>
2020-12-09 15:20:59 -0500

    Adding time and space tests

--- tests/kmer_dict_tests.py
+++ tests/kmer_dict_tests.py
@@ -1,5 +1,7 @@
 import pickle
 import numpy as np
+import os
+import time
 
 def main():
   DATA_PATH = '../data/'
@@ -9,5 +11,36 @@ def main():
     print(f'Length l = {i}\nNumber of keys is {len(kmers.keys())}')
     avg = np.mean([len(kmers[k]) for k in kmers.keys()])
     print(f'Average vals per key is {avg}\n')
+
+
+  print('\n\nTime taken to make each lmer dictionary, with number of proteins = 100')
+
+  for i in [1,2,3,4,5,10]:
+
+      cmd = f'python ../scripts/kmers.py --infile ../data/protein_dict_num_prots_100.pickle -k {i} -n 100 > output'
+      time.sleep(1)
+      start_time = time.time()
+      for _ in range(2): # run only 2 times -> higher than this makes pickle fail
+          # run this command
+          os.system(cmd)
+      end_time = time.time()
+      print(f'Time taken to make dictionary for l = {i}:\t {(end_time - start_time)/2} s')
+
+
+  print('\n\nTime taken to make dictionary for different number of proteins, l = 4')
+
+  for i in [10,100,200,500,1000]:
+
+      cmd = f'python ../scripts/kmers.py --infile ../data/protein_dict_num_prots_1000.pickle -k 4 -n {i} > output'
+      time.sleep(1)
+      start_time = time.time()
+      for _ in range(10): # run 10 times
+            # run this command
+            os.system(cmd)
+      end_time = time.time()
+      print(f'Time taken to make dictionary for n = {i}:\t {(end_time - start_time)/10} s')
+      size = os.path.getsize(DATA_PATH + 'kmer_dict_k_4_num_prots_' + str(i) + '.pickle')
+      print(f'\t\t\tSize of file:\t{size/1024} KB')
+
 
 main()
