e693dadd956dc06f92efb1d516a9b937afb1dab9
Ishita Tripathi <sakurakhadag@gmail.com>
2020-12-09 14:27:21 -0500

    Add 100th read

--- scripts/kmers.py
+++ scripts/kmers.py
@@ -4,11 +4,75 @@ Python program to crete kmers from uniprot fasta file
 Code used for Computational Genomics Projects Fa 2020
 
 Usage:
-python kmers.py --infile uniprot_sprot.fasta -k 5 -n 100
+python kmers.py --infile ../data/uniprot_sprot.pickle -k 5 -n 100
 '''
 
 import argparse
 import pickle
+
+OUTDIR = '../data/'
+
+def read_from_pickle(f):
+    """ Reads a dictionary from file """
+    kmer_dict = {}
+    with open(f, "rb") as fd:
+        dictionary = pickle.load(fd)
+    return dictionary
+
+def read_pickle(protein_dict, k, n):
+    kmers = {} # dictionary to store kmers
+
+    count_proteins = 0 # count of proteins read
+    for protein_id in protein_dict.keys():
+
+        # get protein sequence
+        protein_seq = protein_dict[protein_id]
+
+        # generate all possible kmers
+        for i in range(len(protein_seq) - k + 1):
+            kmer = protein_seq[i:i+k] # could be made faster by windowing
+            if kmer not in kmers:
+                kmers[kmer] = [(protein_id, i)]
+            else:
+                kmers[kmer].append((protein_id, i))
+        count_proteins +=1
+        if n == count_proteins:
+            break
+    return kmers
+
+def read_fasta(lines, k, n, delim):
+    print('Making dict ..')
+
+    seq = ''
+    kmers = {} # dictionary to store kmers
+
+    count_proteins = 0 # count of proteins read
+    for j, line in enumerate(lines):
+        line = line.strip()
+        if (line[0] <'A' or line[0] > 'Z'): # id line of protein
+            # if you've hit the next protein seq, that means you have the
+            # complete last one. Make kmers for it
+            for i in range(len(seq) - k + 1):
+
+                kmer = seq[i:i+k] # could be made faster by windowing
+                if kmer not in kmers:
+                    kmers[kmer] = [(protein_id, i)]
+                else:
+                    kmers[kmer].append((protein_id, i))
+
+            count_proteins +=1
+            if n == count_proteins:
+                break
+
+            # store next protein id
+            protein_id = line.split(delim)[1]
+
+            # initialize seq again
+            seq = ''
+        else:
+            # add to current protein sequence
+            seq += line
+    return kmers
 
 def main():
         ap = argparse.ArgumentParser()
@@ -28,47 +92,30 @@ def main():
 
         delim = args.delimiter
         n = args.n
+        extension = infile.split('.')[-1]
 
-        seq = ''
-        with open(infile) as f:
-            lines = f.readlines()
 
-        if n == None:
-            n = len(lines)
-            outfile = 'kmer_dict_k_' + str(k) + '.pickle'
+        if  extension == 'pickle':
+            database = read_from_pickle(infile)
+        elif extension == 'fasta':
+            with open(infile) as f:
+                database = f.readlines()
         else:
-            outfile = 'kmer_dict_k_' + str(k) + '_num_prots_' + str(n) + '.pickle'
+            print('Please give a .pickle or .fasta infile')
+            exit()
 
+        global OUTDIR
 
-        print('Making dict ..')
-        kmers = {} # dictionary to store kmers
+        if n == None or n>len(database):
+            n = len(database)
+            outfile = OUTDIR + 'kmer_dict_k_' + str(k) + '.pickle'
+        else:
+            outfile = OUTDIR + 'kmer_dict_k_' + str(k) + '_num_prots_' + str(n) + '.pickle'
 
-        count_proteins = 0 # count of proteins read
-        for i, line in enumerate(lines):
-            line = line.strip()
-            if (line[0] <'A' or line[0] > 'Z'): # id line of protein
-                # if you've hit the next protein seq, that means you have the
-                # complete last one. Make kmers for it
-                for i in range(len(seq) - k + 1):
-
-                    kmer = seq[i:i+k] # could be made faster by windowing
-                    if kmer not in kmers:
-                        kmers[kmer] = [(protein_id, i)]
-                    else:
-                        kmers[kmer].append((protein_id, i))
-
-                count_proteins +=1
-                if n == count_proteins:
-                    break
-
-                # store next protein id
-                protein_id = line.split(delim)[1]
-
-                # initialize seq again
-                seq = ''
-            else:
-                # add to current protein sequence
-                seq += line
+        if extension == 'fasta':
+            kmers = read_fasta(database, k, n, delim)
+        else:
+            kmers = read_pickle(database, k, n)
 
         # now store kmers to file using pickle
         print('Dumping dictionary ..')
