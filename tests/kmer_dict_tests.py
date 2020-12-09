1a1f05e484776c34db351a78cdc322c2b95946ab
Ishita Tripathi <sakurakhadag@gmail.com>
2020-12-09 13:55:55 -0500

    Fixing kmer tests file

--- tests/kmer_dict_tests.py
+++ tests/kmer_dict_tests.py
@@ -2,11 +2,12 @@ import pickle
 import numpy as np
 
 def main():
+  DATA_PATH = '../data/'
   for i in [1,2,3,4,5,10]:
-    f = 'kmer_dict_k_' + str(i) + '_num_prots_100.pickle'
+    f = DATA_PATH + 'kmer_dict_k_' + str(i) + '_num_prots_100.pickle'
     with open(f, "rb") as pf: kmers = pickle.load(pf)
     print(f'Length l = {i}\nNumber of keys is {len(kmers.keys())}')
     avg = np.mean([len(kmers[k]) for k in kmers.keys()])
-    print(f'Average vals per key is {avg}')
+    print(f'Average vals per key is {avg}\n')
 
 main()
